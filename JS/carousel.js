e6c12156cc34c6882e1c3a06eea046d6095bd49c
raulauad <raulauad@gmail.com>
2025-09-15 20:28:44 -0300

    -2 Corrijo styles, y botones

--- JS/carousel.js
+++ JS/carousel.js
@@ -4,25 +4,25 @@ const prevBtn = document.getElementById("left");
 const nextBtn = document.getElementById("right");
 
 let index = 0;
-let timer = null;
+let timer = null; //Inicio timer en null, ya que todavia no se inició
 
 function slideWidth() {
-    // El ancho visible del viewport 
+    // El ancho visible del viewport, que es el ancho del contenedor padre
     return track.parentElement.clientWidth;
 }
 
 function goTo(i) {
-    index = (i + slides.length) % slides.length;  // wrap circular
-    const offset = -index * slideWidth();
-    track.style.transform = `translateX(${offset}px)`;
+    index = (i + slides.length) % slides.length; //Sumo slides.length para evitar negativos, uso modulo para volver al inicio
+    const offset = -index * slideWidth(); //Index negativo, ya que muevo la tira a la izquierda * el ancho de la imagen(slideWidth())
+    track.style.transform = `translateX(${offset}px)`; //Aplico el movimiento al contenedor de imagenes
 }
 
 function next() { goTo(index + 1); }
 function prev() { goTo(index - 1); }
 
 function startAuto() {
-    stopAuto();
-    timer = setInterval(next, 3000);
+    stopAuto(); //Limpiamos el timer si ya estaba andando
+    timer = setInterval(next, 3000); //Cada 3 segundos, llamamos a next("devuelve el siguiente valor")
 }
 
 function stopAuto() {
@@ -33,7 +33,7 @@ function stopAuto() {
 // Listeners
 nextBtn.addEventListener("click", () => { next(); startAuto(); });
 prevBtn.addEventListener("click", () => { prev(); startAuto(); });
-window.addEventListener("resize", () => goTo(index)); // Recalcular al cambiar tamaño
+window.addEventListener("resize", () => goTo(index)); //Recalcular al cambiar tamaño
 
 // Init
 goTo(0);
