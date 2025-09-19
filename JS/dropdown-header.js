758a3ce179e40051f205f72117e0a15b1c6ad62a
raulauad <raulauad@gmail.com>
2025-09-19 14:53:39 -0300

    Inicio dropdown-menu horizontal

---
+++ JS/dropdown-header.js
@@ -0,0 +1,31 @@
+const toggleButton = document.querySelector('.dropbtn');
+const dropdownMenu = document.querySelector('.dropdown ul');
+
+let menuOpen = false;
+
+toggleButton.addEventListener('click', () => {
+
+    menuOpen = !menuOpen;
+    if (menuOpen) {
+        dropdownMenu.style.display = 'block';
+        dropdownMenu.style.display = 'flex';
+
+    } else {
+        dropdownMenu.style.display = 'none';
+    }
+});
+
+window.addEventListener('click', (e) => {
+
+    if (!toggleButton.equals(e.target) && !dropdownMenu.contains(e.target)) {
+        dropdownMenu.style.display = 'none';
+        menuOpen = false;
+        toggleButton.classList.remove('open');
+    }
+});
+
+const menuItems = dropdownMenu.querySelectorAll('li');
+
+menuItems.forEach((item, index) => {
+    item.style.transitionDelay = `${index * 0.3}s`;
+})
